b1205197b66e0cd59393a5851708b01529994f8a
Travis Harrison <guumby@gmail.com>
2013-08-09 09:42:00 -0500

    fix missing path

--- dev/load-m5nr.sh
+++ dev/load-m5nr.sh
@@ -7,6 +7,6 @@ DATA_FTP=ftp://ftp.metagenomics.anl.gov/data/M5nr/solr/v${M5NR_VER}/md5_annotati
 DATA_SIZE=500000
 
 # load as we download
+cp dev/chunk_post.pl $SOLR_DIR/example/exampledocs
 cd $SOLR_DIR/example/exampledocs
 wget -q -O - $DATA_FTP | zcat | ./chunk_post.pl $DATA_SIZE
-rm tmp.json
