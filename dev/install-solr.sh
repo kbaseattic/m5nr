948b0783f5cc1464f3286ad2c6c2d6ec0ca2c504
Travis Harrison <guumby@gmail.com>
2013-10-11 10:18:20 -0500

    updated solr version

--- dev/install-solr.sh
+++ dev/install-solr.sh
@@ -8,10 +8,10 @@ if [[ $# -gt 0 ]] ; then
 fi
 
 # install solr
-wget http://apache.mirrors.hoobly.com/lucene/solr/4.4.0/solr-4.4.0.tgz
-tar -xzf solr-4.4.0.tgz -C $target
-ln -s $target/solr-4.4.0 $target/solr
-rm solr-4.4.0.tgz
+wget http://apache.mirrors.hoobly.com/lucene/solr/4.5.0/solr-4.5.0.tgz
+tar -xzf solr-4.5.0.tgz -C $target
+ln -s $target/solr-4.5.0 $target/solr
+rm solr-4.5.0.tgz
 
 # init.d file
 tpage --define target=$target solr.tt > /etc/init.d/solr
